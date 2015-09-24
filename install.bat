8414e84053ce94c8965d6d97ad1d915db36afcf9
lvyahui <lvyahui8@126.com>
2015-09-24 10:56:05 -0700

    add install scripts

---
+++ install.bat
@@ -0,0 +1,3 @@
+@ehco off
+copy /Y C:\Windows\System32\drivers\etc\hosts C:\Windows\System32\drivers\etc\hosts.bak
+copy /Y hosts C:\Windows\System32\drivers\etc\hosts
---
+++ install.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+mv -f /etc/hosts /etc/hosts.bak
+cp -f hosts /etc/hosts 
